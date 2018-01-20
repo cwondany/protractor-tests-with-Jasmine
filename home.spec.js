056eec3d5c5d82f160ce68ff08fb50f5c4fbbed8
Chiara Wondany <cwondany@apollon.de>
2018-01-20 19:19:08 +0100

    gitignore for .idea/ node_modules added

--- home.spec.js
+++ home.spec.js
@@ -1,5 +1,36 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+var driver = new webdriver.Builder().forBrowser('chrome').build();
+//.get
+driver.get('https://library-app.firebaseapp.com/');
+
+
+var submitBtn = driver.findElement(By.css('.btn-lg'));
+driver.findElement(By.css('input')).sendKeys('us');
+
+//(+) explicit wait: .wait
+driver.wait(function () {
+    return submitBtn.getCssValue('opacity').then(function (result) {
+
+        return result === '1';
+    })
+}, 15000);
+
+submitBtn.click();
+
+//(+) explicit wait: .until
+driver.wait(until.elementLocated(By.css('.alert-success')), 5000).getText().then(function (txt) {
+    console.log('Alert success text is: ' + txt);
+});
+
+
+//cmd: webdriver-manager start
+
+/*
 describe('default_toBetrue ', function () {
     it('ShouldCheckSomething', function () {
         expect(true).toBe(true);
     })
-});
+});*/
